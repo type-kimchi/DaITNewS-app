6ef5d45324d9a44b4bef42bd18dd0ab7fcc69a94
sangwonchoi <swgood4@gmail.com>
2025-07-31 17:20:27 +0900

    Fix: Bootstrap 5 navbar toggler attributes

--- client/src/App.js
+++ client/src/App.js
@@ -53,7 +53,7 @@ function App() {
     <div className="App">
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
         <Link className="navbar-brand" to="/">DaITNewS</Link>
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarNav">
